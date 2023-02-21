0d15ad9fbce4387cb1d339b1d01ea7b3ae6e767a
Alex Howard <alex-m-howard@pm.me>
2023-02-20 19:54:51 -0500

    Added boilerplate routes

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function test() {
+  return <h1>HELLO WORLD</h1>;
+}
+
+export default test;
---
+++ frontend/src/pages/profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function test() {
+  
+
+  return (
+    <h1>HELLO WORLD</h1>
+  )
+}
+
+export default test;
