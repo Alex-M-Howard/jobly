1ae61ba67742be732af07dcb6f52cc917e6c9fad
Alex Howard <alex-m-howard@pm.me>
2023-02-20 19:53:40 -0500

    Added MUI for styling

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../config/theme";
+import createEmotionCache from "../config/createEmotionCache";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme={true} />
+          <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
