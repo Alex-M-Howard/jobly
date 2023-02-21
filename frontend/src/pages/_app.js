09d7aaa612911155050172544e0a20601530e4b9
Alex Howard <alex-m-howard@pm.me>
2023-02-20 21:58:28 -0500

    Added MUI Icons

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,8 +1,10 @@
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { CacheProvider } from "@emotion/react";
-import theme from "../config/theme";
-import createEmotionCache from "../config/createEmotionCache";
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../config/theme';
+import createEmotionCache from '../config/createEmotionCache';
+import NavBar from '@/components/NavBar'
+
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
