3f7319cfd9fdde007c9a39892271c059b6107022
Alex Howard <alex-m-howard@pm.me>
2023-02-22 15:38:28 -0500

    Added Roboto Font

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -5,6 +5,10 @@ import { CacheProvider } from "@emotion/react";
 import { lightTheme, darkTheme } from "../config/theme";
 import createEmotionCache from "../config/createEmotionCache";
 import NavBar from "@/components/NavBar";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
