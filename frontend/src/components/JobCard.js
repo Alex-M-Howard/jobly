40077c247331eab975df5496b49a6e51680f2832
Alex Howard <alex-m-howard@pm.me>
2023-02-22 18:05:48 -0500

    Split job cards into own component for readability

---
+++ frontend/src/components/JobCard.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Grid,
+  Box,
+  Button,
+} from "@mui/material";
+
+  function JobCard({logo, companyName, title, salary, theme}){
+    return (
+      <Grid item xs={12} sm={8} md={6} lg={4}>
+        <Card variant="outlined" sx={{ maxWidth: 425 }}>
+          <Card
+            sx={{ display: "flex" }}
+            style={{ backgroundColor: `${theme.palette.secondary.main}` }}>
+            <CardMedia
+              component="img"
+              sx={{ width: 125 }}
+              image={
+                logo
+                  ? logo
+                  : "https://previews.123rf.com/images/doublerdesign/doublerdesign1911/doublerdesign191100109/133223058-simple-building-icon-logo-design-inspiration-vector-illustration-template.jpg"
+              }
+              alt={companyName}
+            />
+            <Box
+              sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <div>
+                  <Typography
+                    component="div"
+                    variant="h6"
+                    style={{
+                      fontWeight: "bold",
+                      color: `${theme.palette.text.main}`,
+                    }}>
+                    {companyName}
+                  </Typography>
+                </div>
+                <Typography
+                  variant="subtitle1"
+                  style={{
+                    color: `${theme.palette.text.main}`,
+                    marginBottom: "2px",
+                  }}
+                  component="div">
+                  {title}
+                </Typography>
+
+                <Typography
+                  justifyContent="center"
+                  style={{
+                    color: `${theme.palette.text.main}`,
+                    fontStyle: "italic",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    position: "relative",
+                    top: "15px",
+                  }}>
+                  <Button
+                    variant="outlined"
+                    style={{ color: `${theme.palette.accent.main}` }}>
+                    Apply
+                  </Button>
+                  {salary ? "$" + salary : ""}
+                </Typography>
+              </CardContent>
+            </Box>
+          </Card>
+        </Card>
+      </Grid>
+    );
+  }
+
+export default JobCard;
--- frontend/src/pages/jobs/index.js
+++ frontend/src/pages/jobs/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import JoblyApi from "@/API";
-import { Card, CardContent, CardMedia, Typography, Grid, Box } from "@mui/material";
+import { Grid } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import JobCard from "@/components/JobCard";
 
 function Jobs() {
   const theme = useTheme();
@@ -18,51 +19,13 @@ function Jobs() {
 
   const jobCards = jobs.map((job) => {
     return (
-      <Grid item xs={12} sm={8} md={6} lg={4}>
-        <Card variant="outlined" sx={{ maxWidth: 425 }}>
-          <Card
-            sx={{ display: "flex" }}
-            style={{ backgroundColor: `${theme.palette.secondary.main}` }}>
-            <CardMedia
-              component="img"
-              sx={{ width: 125 }}
-              image={
-                job.logo
-                  ? job.logo
-                  : "https://previews.123rf.com/images/doublerdesign/doublerdesign1911/doublerdesign191100109/133223058-simple-building-icon-logo-design-inspiration-vector-illustration-template.jpg"
-              }
-              alt={job.companyName}
-            />
-            <Box
-              sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
-              <CardContent sx={{ flex: "1 0 auto" }}>
-                <div>
-                  <Typography
-                    component="div"
-                    variant="h6"
-                    style={{
-                      fontWeight: "bold",
-                      color: `${theme.palette.text.main}`,
-                    }}>
-                    {job.companyName}
-                  </Typography>
-                </div>
-                <Typography
-                  variant="subtitle1"
-                  style={{ color: `${theme.palette.text.main}`, marginBottom: '2px' }}
-                  component="div">
-                  {job.title}
-                </Typography>
-                <Typography
-                  align="right"
-                  style={{ color: `${theme.palette.text.main}`, fontStyle: 'italic'}}>
-                  {job.salary ? "$" + job.salary : ""}
-                </Typography>
-              </CardContent>
-            </Box>
-          </Card>
-        </Card>
-      </Grid>
+      <JobCard
+        logo={job.logo}
+        title={job.title}
+        companyName={job.companyName}
+        salary={job.salary}
+        theme={theme}
+      />
     );
   });
 
@@ -73,7 +36,8 @@ function Jobs() {
         spacing={2}
         justifyContent="center"
         alignItems="stretch"
-        style={{ marginTop: "5px", padding: "10px" }}>
+        style={{  padding: "10px", backgroundColor: `${theme.palette.background.main}`,
+         }}>
         {jobCards}
       </Grid>
     </div>
