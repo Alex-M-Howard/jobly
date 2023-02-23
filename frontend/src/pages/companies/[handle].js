39884de0d5ed5e31b81478f4f6c4943dd849bede
Alex Howard <alex-m-howard@pm.me>
2023-02-22 20:41:22 -0500

    Added route for seeing individual company

--- frontend/src/pages/companies/[handle].js
+++ frontend/src/pages/companies/[handle].js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useRouter } from "next/router";
+import JoblyApi from "@/API";
+import { CircularProgress, Grid } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import CompanyInfoCard from "@/components/CompanyInfoCard";
 
-function test() {
-  return <h1>search by a company name</h1>;
-}
+function CompanyInfo() {
+  const [company, setCompany] = useState()
+  const router = useRouter();
+  const theme = useTheme();
+  const companyHandle = router.query.handle;
 
-export default test;
+  useEffect(function fetchWhenMounted() {
+    async function fetchData() {
+      let data = await JoblyApi.getCompany(companyHandle);
+      setCompany(data)
+    }
+
+    fetchData();
+    
+  }, [companyHandle]);
+  
+  if (!company) {
+      return (
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          style={{  }}>
+          <Grid item>
+            <CircularProgress
+              size={70}
+              style={{ color: `${theme.palette.secondary.main}` }}
+            />
+          </Grid>
+        </Grid>
+      );
+    }
+      
+    return (
+        <CompanyInfoCard 
+        name={company.name} 
+        description={company.description} 
+        numEmployees={company.numEmployees} 
+        jobs={company.jobs}
+        theme={theme}
+        />
+      );
+  }
+
+export default CompanyInfo;
