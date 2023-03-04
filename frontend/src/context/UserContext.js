8ba07708af8f8d74772bb80c33a384ba6551e347
Alex Howard <alex-m-howard@pm.me>
2023-03-04 16:42:49 -0500

    Added user object to UserContext

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -4,13 +4,14 @@ export const UserContext = createContext();
 
 const UserContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({ username: null})
 
   const toggleLoginStatus = () => {
     setIsLoggedIn((state) => !state);
   };
 
   return (
-    <UserContext.Provider value={{ isLoggedIn, toggleLoginStatus }}>
+    <UserContext.Provider value={{ isLoggedIn, toggleLoginStatus, user, setUser }}>
       {children}
     </UserContext.Provider>
   );
