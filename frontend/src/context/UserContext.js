fff79d2ddb02ea3f7b3e84a685af6d7c5698569d
Alex Howard <alex-m-howard@pm.me>
2023-03-04 16:52:12 -0500

    Checks local storage when starting app for user

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -1,17 +1,32 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
+import JoblyApi from "@/API";
 
 export const UserContext = createContext();
 
 const UserContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState({ username: null})
+  const [user, setUser] = useState({ username: null });
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+      setUser({username: localStorage.getItem("username")})
+      // Fetch user data using token and set the user state
+      // Example:
+      // let res = async () => {
+      //   await JoblyApi.getUser(token).then((userData) => setUser(userData))
+      // }
+    }
+  }, []);
 
   const toggleLoginStatus = () => {
     setIsLoggedIn((state) => !state);
   };
 
   return (
-    <UserContext.Provider value={{ isLoggedIn, toggleLoginStatus, user, setUser }}>
+    <UserContext.Provider
+      value={{ isLoggedIn, toggleLoginStatus, user, setUser }}>
       {children}
     </UserContext.Provider>
   );
