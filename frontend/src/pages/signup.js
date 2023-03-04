6bc41456db1224c5c7f711618a551c57dba9405b
Alex Howard <alex-m-howard@pm.me>
2023-03-04 16:09:20 -0500

    Added signup component

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,7 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
+import Form from "@/components/Form";
+import { Grid, Alert, AlertTitle } from "@mui/material";
+import JoblyApi from "@/API";
 
-function test() {
-  return <h1>This will be the signup page</h1>;
+function Signup() {
+  const [error, setError] = useState(null)
+
+  const fields = [
+    { name: 'username', label: 'Username' },
+    { name: 'password', label: 'Password' },
+    { name: 'firstName', label: 'First Name'},
+    {name: 'lastName', label: 'Last Name'},
+    {name: 'email', label: 'Email'}
+  ]
+  
+  const initialValues = {
+      username: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+      email: ''
+    }
+  
+  const handleSubmit = async (formData) => {
+    if (error) setError(null);
+
+    let res = await JoblyApi.registerUser(formData)
+    
+    if (res.error) {
+        setError(res.error);
+    }
+  }
+
+  
+  return (
+    <Grid
+      container
+      direction='column'
+      justifyContent='center'
+      alignItems='center'
+    >
+      {error !== null ? (
+        <Alert sx={{m: 2}} severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {error}
+        </Alert>
+      ) : null}
+      <Form
+        fields={fields}
+        initialValues={initialValues}
+        handleSubmit={handleSubmit}
+        buttonText='Sign Up'
+      />
+    </Grid>
+  );
 }
 
-export default test;
+export default Signup;
