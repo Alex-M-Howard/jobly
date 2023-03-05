8973c8cd59f3621c926f3f2c3fcef3c39d8ad904
Alex Howard <alex-m-howard@pm.me>
2023-03-04 19:32:48 -0500

    Added theme to local storage

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
@@ -10,17 +10,38 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import UserContextProvider from "@/context/UserContext";
+import Loading from "@/components/Loading";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-  const [theme, setTheme] = useState(lightTheme);
+  const [theme, setTheme] = useState(null);
 
   const toggleTheme = () => {
     setTheme(theme === lightTheme ? darkTheme : lightTheme);
   };
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      setTheme(darkTheme);
+    } else {
+      setTheme(lightTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme) {
+      let mode = theme.palette.mode;
+      localStorage.setItem("theme", mode);
+    }
+  }, [theme])
+
+  if (!theme) {
+    return 'Loading'
+  }
 
   return (
     <CacheProvider value={emotionCache}>
