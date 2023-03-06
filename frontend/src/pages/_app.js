b5bd0c082837c885181b5a5ac323981a67895909
Alex Howard <alex-m-howard@pm.me>
2023-03-05 19:55:01 -0500

    Moved context to different folder

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
-import { lightTheme, darkTheme } from "../config/theme";
+import { lightTheme, darkTheme } from "@/context/theme";
 import createEmotionCache from "../config/createEmotionCache";
 import NavBar from "@/components/NavBar";
 import "@fontsource/roboto/300.css";
@@ -10,7 +10,6 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import UserContextProvider from "@/context/UserContext";
-import Loading from "@/components/Loading";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
