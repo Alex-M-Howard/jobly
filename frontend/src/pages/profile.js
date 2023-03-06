72877aa951501be28fe0885b94b2758c8942d326
Alex Howard <alex-m-howard@pm.me>
2023-03-05 19:54:27 -0500

    Added profile component

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import Form from "@/components/Form";
+import { Grid, Alert, AlertTitle, Typography } from "@mui/material";
+import { UserContext } from "@/context/UserContext";
+import JoblyApi from "@/API";
 
-function test() {
-  
+function Profile() {
+  const [error, setError] = useState(null);
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return null;
+  }
+
+  const fields = [
+    { name: "firstName", label: "First Name" },
+    { name: "lastName", label: "Last Name" },
+    { name: "email", label: "Email" },
+  ];
+
+  const initialValues = {
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+  };
+
+  const handleSubmit = async (formData) => {
+    if (error) setError(null);
+
+    let res = await JoblyApi.registerUser(formData);
+
+    if (res.error) {
+      setError(res.error);
+    }
+  };
 
   return (
-    <h1>This will be profile page where you can edit your info.</h1>
-  )
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center">
+      {error !== null ? (
+        <Alert
+          sx={{ m: 2, minWidth: "350px", maxWidth: "350px" }}
+          severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {error}
+        </Alert>
+      ) : null}
+      <Typography align="center" variant="h3" sx={{ mt: 5 }}>
+        {`${user.username}'s`} Profile
+      </Typography>
+
+      <Form
+        fields={fields}
+        initialValues={initialValues}
+        handleSubmit={handleSubmit}
+        buttonText="Save"
+      />
+    </Grid>
+  );
 }
 
-export default test;
+
+export default Profile;
